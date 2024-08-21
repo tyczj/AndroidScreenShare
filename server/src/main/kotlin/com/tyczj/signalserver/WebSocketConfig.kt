77edc4cee71e62f0df11fbdd7e54271f6876cb5c
tyczj <tyczj359@gmail.com>
2024-08-20 22:03:53 -0400

    Increased max text buffer size

--- server/src/main/kotlin/com/tyczj/signalserver/WebSocketConfig.kt
+++ server/src/main/kotlin/com/tyczj/signalserver/WebSocketConfig.kt
@@ -1,10 +1,12 @@
 package com.tyczj.signalserver
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
 
 
 @Configuration
@@ -13,5 +15,12 @@ class WebSocketConfig(private val webSocketHandler: WebSocketHandler) : WebSocke
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(webSocketHandler, "/").setAllowedOrigins("*")
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean {
+        val factoryBean = ServletServerContainerFactoryBean()
+        factoryBean.setMaxTextMessageBufferSize(10000 * 1024)
+        return factoryBean
     }
 }
